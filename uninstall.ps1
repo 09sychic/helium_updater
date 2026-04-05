929025ac0b94b14bb700bc276c1d2f5938ec239d
Drae <bwakang1212@gmail.com>
2026-04-06 01:13:53 +0800

    Update uninstall.ps1

--- uninstall.ps1
+++ uninstall.ps1
@@ -1,6 +1,6 @@
 # Removes the executable from Startup, and deletes the helium_updater app data folder.
 
-$StartupExe = Join-Path ([System.Environment]::GetFolderPath("Startup")) "helium_updater.exe"
+$StartupExe = Join-Path ([System.Environment]::GetFolderPath("Startup")) "09sychic-HeliumSync.exe"
 $CacheDir = Join-Path $env:LOCALAPPDATA "helium_updater"
 
 Remove-Item -Force $StartupExe -ErrorAction SilentlyContinue
